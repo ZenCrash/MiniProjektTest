465f7b466c5e037dad0c7856806827fe69074edb
VigtorWolf <73114751+VigtorWolf@users.noreply.github.com>
2021-02-25 12:47:53 +0100

    Create UnitTestClass.java

---
+++ src/test/UnitTestClass.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UnitTestClass {
+	@Before
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	@After
+
+}
