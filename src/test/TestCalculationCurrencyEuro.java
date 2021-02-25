ea421b118f6c17afc90f1a0c21b38a42d6174446
Zixlar <73934331+Zixlar@users.noreply.github.com>
2021-02-25 10:33:29 +0100

    Euro test færdig

--- src/test/TestCalculationCurrencyEuro.java
+++ src/test/TestCalculationCurrencyEuro.java
@@ -21,9 +21,6 @@ public class TestCalculationCurrencyEuro {
 		ps = new ControlPayStation();
 	}
 	
-	/**
-	 * Entering no coins should make the display report 0 minutes parking time.
-	 */
 	
 	@Test
 	public void shouldDisplay6minutesFor1Euro() throws IllegalCoinException {
@@ -58,6 +55,97 @@ public class TestCalculationCurrencyEuro {
 		ps.addPayment(coinValue, coinCurrency, coinType);
 		// Assert
 		assertEquals("Should display 2 min for 5cent", expectedParkingTime, ps.readDisplay());
+	}
+	
+	@Test
+	public void shouldDisplay80MinFor2Euro() throws IllegalCoinException {
+		
+		// Arrange
+		int expectedParkingTime = 80;	// In minutes		
+		int coinValue = 2;
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+		
+		// Act
+		ps.addPayment(coinValue, coinCurrency, coinType);
+		// Assert
+		assertEquals("Should display 80 min for 2Euro", expectedParkingTime, ps.readDisplay());
+	}
+	
+	@Test
+	public void shouldDisplay1MinFor2Cents() throws IllegalCoinException {
+		
+		// Arrange
+		int expectedParkingTime = 1;	// In minutes		
+		int coinValue = 2;
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.FRACTION;
+		
+		// Act
+		ps.addPayment(coinValue, coinCurrency, coinType);
+		// Assert
+		assertEquals("Should display 1 min for 2cent", expectedParkingTime, ps.readDisplay());
+	}
+	
+	@Test
+	public void shouldDisplay4MinFor10Cents() throws IllegalCoinException {
+		
+		// Arrange
+		int expectedParkingTime = 4;	// In minutes		
+		int coinValue = 10;
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.FRACTION;
+		
+		// Act
+		ps.addPayment(coinValue, coinCurrency, coinType);
+		// Assert
+		assertEquals("Should display 4 min for 10cent", expectedParkingTime, ps.readDisplay());
+	}
+	
+	@Test
+	public void shouldDisplay8MinFor20Cents() throws IllegalCoinException {
+		
+		// Arrange
+		int expectedParkingTime = 8;	// In minutes		
+		int coinValue = 20;
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.FRACTION;
+		
+		// Act
+		ps.addPayment(coinValue, coinCurrency, coinType);
+		// Assert
+		assertEquals("Should display 8 min for 20cent", expectedParkingTime, ps.readDisplay());
+	}
+	@Test
+	public void shouldDisplay20MinFor50Cents() throws IllegalCoinException {
+		
+		// Arrange
+		int expectedParkingTime = 20;	// In minutes		
+		int coinValue = 50;
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.FRACTION;
+		
+		// Act
+		ps.addPayment(coinValue, coinCurrency, coinType);
+		// Assert
+		assertEquals("Should display 8 min for 20cent", expectedParkingTime, ps.readDisplay());
+	}
+	@Test
+	public void shouldDisplay41MinFor1Cents1Euro() throws IllegalCoinException {
+		
+		// Arrange
+		int expectedParkingTime = 41;	// In minutes		
+		int coinValueCent = 1;
+		int coinValueEuro = 1;
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
+		Currency.ValidCoinType coinTypeFRAC = Currency.ValidCoinType.FRACTION;
+		Currency.ValidCoinType coinTypeINT = Currency.ValidCoinType.INTEGER;
+		
+		// Act
+		ps.addPayment(coinValueCent, coinCurrency, coinTypeFRAC);
+		ps.addPayment(coinValueEuro, coinCurrency, coinTypeINT);
+		// Assert
+		assertEquals("Should display 8 min for 20cent", expectedParkingTime, ps.readDisplay());
 	}
 
 	
