7a452843f2f95ed76b43a593b8a4d09bdd9a2173
VigtorWolf <73114751+VigtorWolf@users.noreply.github.com>
2021-02-25 14:17:41 +0100

    Update UnitTestClass.java

--- src/test/UnitTestClass.java
+++ src/test/UnitTestClass.java
@@ -2,15 +2,70 @@ package test;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.*;
+
+import controllayer.ControlPrice;
+import databaselayer.DatabaseLayerException;
+import databaselayer.DatabasePBuy;
+import databaselayer.DatabasePPrice;
+import databaselayer.IDbPPrice;
+import modellayer.Currency;
+import modellayer.PPrice;
+import modellayer.PZone;
+
+
 
 public class UnitTestClass {
+	private IDbPPrice dbPrice;
+
 	@Before
+	public void setUp() {
+		this.dbPrice = new DatabasePPrice();
+		
+	
+	}
+	@Test
+	public void testPriceByZoneID() throws DatabaseLayerException {
+		//arrange
+		int expectedPrice = 25;
+		PZone zone = new PZone();
+		int zoneID = zone.getpZoneId();
+		System.out.println(zoneID);
+		
+		
+		//act
+		
+		PPrice price = dbPrice.getPriceByZoneId(zoneID);
+		System.out.println(zoneID);
+		
+		System.out.println(price);
+		
+		//assert
+		
+		int p = price.getParkingPrice();
+		System.out.println(p);
+		
+		assertEquals(p, expectedPrice);
+	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void t() throws DatabaseLayerException {
+		//arrange
+		
+		
+		//act
+		
+		
+		
+		//assert
+		
+		
+		
+		//assertEquals();
 	}
+	
 	@After
-
+	public void cleanUp() {
+		
+	}
 }
